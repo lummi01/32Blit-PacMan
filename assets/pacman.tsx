309bd11f310a3291bfa1fd4fff58bf53508140dc
Lummi <72337143+lummi01@users.noreply.github.com>
2025-08-17 13:00:45 +0200

    Delete assets directory

--- assets/pacman.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="spritesheet" tilewidth="8" tileheight="8" tilecount="288" columns="16">
- <image source="spritesheet.png" width="128" height="128"/>
-</tileset>
